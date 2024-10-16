174bf16551bc3cf3a73011ce3a9f966f9e5a4b3b
Harsh Gupta <2k22.cse.2213543@gmail.com>
2024-10-16 12:18:23 +0530

    1700-Number-of-Students-Unable-to-Eat-Lunch.py

--- 1700-Number-of-Students-Unable-to-Eat-Lunch/1700-Number-of-Students-Unable-to-Eat-Lunch.py
+++ 1700-Number-of-Students-Unable-to-Eat-Lunch/1700-Number-of-Students-Unable-to-Eat-Lunch.py
@@ -1,6 +1,9 @@
 # LeetCode Problem: Number of Students Unable to Eat Lunch
 # Problem Link: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
 
+
+
+# Approach 1: Using a queue and stack to simulate the lunch process
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         n=len(students)
@@ -14,3 +17,19 @@ class Solution:
                 students.append(students.pop(0))
                 count+=1
         return len(students)
+
+# Approach 2: Count students who prefer each sandwich type
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        d={0:0,1:0}
+        for i in students:
+            if i in d:
+                d[i]+=1
+        cnt=0
+        for i in sandwiches:
+            if d[i] > 0:
+                d[i] -= 1
+            else:
+                break
+        return d[0] + d[1]
+
