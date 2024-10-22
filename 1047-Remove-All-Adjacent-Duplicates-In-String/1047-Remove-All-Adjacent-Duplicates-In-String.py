392873e15d00a9aaed30f42c052d78227389ed3a
Harsh Gupta <2k22.cse.2213543@gmail.com>
2024-10-22 15:36:28 +0530

    1047-Remove-All-Adjacent-Duplicates-In-String.py

--- 1047-Remove-All-Adjacent-Duplicates-In-String/1047-Remove-All-Adjacent-Duplicates-In-String.py
+++ 1047-Remove-All-Adjacent-Duplicates-In-String/1047-Remove-All-Adjacent-Duplicates-In-String.py
@@ -1,19 +1,15 @@
 # LeetCode Problem: Remove All Adjacent Duplicates In String
-# Problem Link: https://leetcode.com/problems/check-if-n-and-its-double-exist/
+# Problem Link: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
 
 class Solution:
-    def checkIfExist(self, arr: List[int]) -> bool:
-        d = {}
-        for i in arr:
-            if i * 2 in d:
-                d[i * 2] += 1
+    def removeDuplicates(self, s: str) -> str:
+        stack=[]
+        for i in s:
+            if len(stack) and i==stack[-1]:
+                stack.pop()
             else:
-                d[i * 2] = 1
-        for i in arr:
-            if i == 0:
-                if d[0] > 1:
-                    return True
-            else:
-                if i in d:
-                    return True
-        return False
+                stack.append(i)
+        ans=""
+        for i in stack:
+            ans+=i
+        return ans
