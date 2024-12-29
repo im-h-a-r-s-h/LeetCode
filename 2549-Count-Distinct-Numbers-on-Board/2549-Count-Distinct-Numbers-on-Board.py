9d385e661558d86c110ffd2a5cdf9e294b9a5a99
Harsh Gupta <2k22.cse.2213543@gmail.com>
2024-12-29 19:31:08 +0530

    2549-Count-Distinct-Numbers-on-Board.py

---
+++ 2549-Count-Distinct-Numbers-on-Board/2549-Count-Distinct-Numbers-on-Board.py
@@ -0,0 +1,5 @@
+# LeetCode Problem: 2549-Count-Distinct-Numbers-on-Board
+# Problem Link: https://leetcode.com/problems/longest-palindrome/description/
+class Solution:
+    def distinctIntegers(self, n: int) -> int:
+        return max(1,n-1)
